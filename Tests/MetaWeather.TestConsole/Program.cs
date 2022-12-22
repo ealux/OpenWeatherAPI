b51edacdc7b037a0cc4b0120cdee3fde7c9439ec
Егор Люханов <ealuhanov@yandex.ru>
2022-12-22 13:06:38 +0500

    Create and configure Host

--- Tests/MetaWeather.TestConsole/Program.cs
+++ Tests/MetaWeather.TestConsole/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace MetaWeather.TestConsole
+{
+    internal class Program
+    {
+        #region IHost
+
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting
+            ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        // Services
+        public static IServiceProvider Services => Hosting.Services;
+
+        // Create host builder
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        // Create services
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+        }
+
+        #endregion IHost
+
+        private static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Completed!");
+            Console.Read();
+
+            await host.StopAsync();
+        }
+    }
+}

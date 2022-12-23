ec9e0b2271a385801892a711e13686e90ee17593
Егор Люханов <ealuhanov@yandex.ru>
2022-12-23 18:11:25 +0500

    Structure for API usage

---
+++ UI/OpenWeatherAPI.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenWeatherAPI.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

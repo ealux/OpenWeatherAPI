18ccf67a99bb9e47c43bde2e7a2e6095f60039bf
Егор Люханов <ealuhanov@yandex.ru>
2022-12-25 16:44:21 +0500

    Interface basic. IRepository

---
+++ OpenWeatherAPI.Usage/Services/OpenWeatherAPI.Interfaces.Base/Repositories/IPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenWeatherAPI.Interfaces.Base.Repositories
+{
+    public interface IPage<T>
+    {
+        IEnumerable<T> Items { get; }
+
+        int TotalCount { get; }
+        int PageIndex { get; }
+        int PageSize { get; }
+
+        int TotalPagesCount => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
---
+++ OpenWeatherAPI.Usage/Services/OpenWeatherAPI.Interfaces.Base/Repositories/IRepository.cs
@@ -0,0 +1,39 @@
+﻿using OpenWeatherAPI.Interfaces.Base.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OpenWeatherAPI.Interfaces.Base.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        #region [Base]
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(int id);
+
+        Task<T> Add(T item);
+
+        Task<T> Update(T item);
+
+        Task<T> Delete(T item);
+
+        #endregion [Base]
+
+        #region [Extensions]
+
+        Task<int> GetCount();
+
+        Task<bool> ExistsById(int id);
+
+        Task<bool> Exists(T item);
+
+        Task<IEnumerable<T>> Get(int skip, int count);
+
+        Task<IPage<T>> GetPage(int pageIndex, int pageSize);
+
+        Task<T> DeleteById(int id);
+
+        #endregion [Extensions]
+    }
+}

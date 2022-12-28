049522c6d44354e5388d8bfb97642461213ba1b3
Егор Люханов <ealuhanov@yandex.ru>
2022-12-28 21:05:34 +0500

    Business logic data models

---
+++ OpenWeatherAPI.Usage/Common/OpenWeatherAPI.Domain.Base/DataSourceInfo.cs
@@ -0,0 +1,13 @@
+﻿using OpenWeatherAPI.Interfaces.Base.Entities;
+
+namespace OpenWeatherAPI.Domain.Base
+{
+    public class DataSourceInfo : INamedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ OpenWeatherAPI.Usage/Common/OpenWeatherAPI.Domain.Base/DataValueInfo.cs
@@ -0,0 +1,16 @@
+﻿using OpenWeatherAPI.Interfaces.Base.Entities;
+using System;
+
+namespace OpenWeatherAPI.Domain.Base
+{
+    public class DataValueInfo : IEntity
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Time { get; set; }
+
+        public string Value { get; set; }
+
+        public bool IsFault { get; set; }
+    }
+}
